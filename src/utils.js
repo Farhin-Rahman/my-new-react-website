9b2a8bc00f333ce3b543ab8c9de50f3af8cae34a
Farhin Rahman <farhinrahman1998@gmail.com>
2023-10-31 23:51:33 +0600

    my react project done

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+// utils.js
+
+function getImageUrl(imageName) {
+    return `C:\Users\User\new-react-website\public\images/${imageName}`;
+  }
+  
+  // export the function so that it can be used in other files
+  export default getImageUrl;
+  
